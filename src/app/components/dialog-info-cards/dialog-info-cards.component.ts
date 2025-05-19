72f1d88cbc8fe92b45c012ca67024170f3a6f817
azenyr <61697668+azenyr@users.noreply.github.com>
2025-05-19 19:16:47 +0100

    redesigned top cards

--- src/app/components/dialog-info-cards/dialog-info-cards.component.ts
+++ src/app/components/dialog-info-cards/dialog-info-cards.component.ts
@@ -16,13 +16,7 @@ export interface DialogData {
   styleUrl: './dialog-info-cards.component.scss',
 })
 export class DialogInfoCardsComponent {
-  videoCardsUrls = {
-    card1: '',
-    card2: '',
-    card3: '',
-    card4: '',
-    card5: '',
-  };
+  currentCardDataFromTranslate: any[] = new Array(); // array containing all the cards data to show on popup
 
   constructor(public dialogRef: MatDialogRef<DialogInfoCardsComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData, private translateService: TranslateService) {}
 
@@ -30,24 +24,20 @@ export class DialogInfoCardsComponent {
     const currentLang = this.translateService.currentLang ?? 'en';
     const currentLangData = this.translateService.translations[currentLang];
 
-    this.videoCardsUrls.card1 = currentLangData?.CARDS?.CARD1?.VIDEO_URL ?? '';
-    this.videoCardsUrls.card2 = currentLangData?.CARDS?.CARD2?.VIDEO_URL ?? '';
-    this.videoCardsUrls.card3 = currentLangData?.CARDS?.CARD3?.VIDEO_URL ?? '';
-    this.videoCardsUrls.card4 = currentLangData?.CARDS?.CARD4?.VIDEO_URL ?? '';
-    this.videoCardsUrls.card5 = currentLangData?.CARDS?.CARD5?.VIDEO_URL ?? '';
+    this.currentCardDataFromTranslate = currentLangData?.CARDS[this.data.infoCardId]?.INNER_CARDS ?? [];
 
-    this.translateService.onLangChange.subscribe({
-      next: (newLangData: TranslationChangeEvent) => {
-        this.videoCardsUrls.card1 = newLangData.translations?.CARDS?.CARD1?.VIDEO_URL ?? '';
-        this.videoCardsUrls.card2 = newLangData.translations?.CARDS?.CARD2?.VIDEO_URL ?? '';
-        this.videoCardsUrls.card3 = newLangData.translations?.CARDS?.CARD3?.VIDEO_URL ?? '';
-        this.videoCardsUrls.card4 = newLangData.translations?.CARDS?.CARD4?.VIDEO_URL ?? '';
-        this.videoCardsUrls.card5 = newLangData.translations?.CARDS?.CARD5?.VIDEO_URL ?? '';
-      },
-      error: error => {
-        console.log(error);
-      },
-    });
+    // this.translateService.onLangChange.subscribe({
+    //   next: (newLangData: TranslationChangeEvent) => {
+    //     this.videoCardsUrls.card1 = newLangData.translations?.CARDS?.CARD1?.VIDEO_URL ?? '';
+    //     this.videoCardsUrls.card2 = newLangData.translations?.CARDS?.CARD2?.VIDEO_URL ?? '';
+    //     this.videoCardsUrls.card3 = newLangData.translations?.CARDS?.CARD3?.VIDEO_URL ?? '';
+    //     this.videoCardsUrls.card4 = newLangData.translations?.CARDS?.CARD4?.VIDEO_URL ?? '';
+    //     this.videoCardsUrls.card5 = newLangData.translations?.CARDS?.CARD5?.VIDEO_URL ?? '';
+    //   },
+    //   error: error => {
+    //     console.log(error);
+    //   },
+    // });
   }
 
   onNoClick(): void {
