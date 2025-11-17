544066272b78b347582000475c8ae93e0b74a00b
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2025-11-17 10:38:56 +0000

    feat: move trust-by section to it's own component

---
+++ src/app/components/trusted-by/trusted-by.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trusted-by',
+  styleUrl: './trusted-by.component.scss',
+  templateUrl: './trusted-by.component.html',
+})
+export class TrustedByComponent {
+  constructor() {}
+}
