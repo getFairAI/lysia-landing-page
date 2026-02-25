6aab68a7bd6a2448ce133224c8f24d384ffabaa7
l-silvestre <43357028+l-silvestre@users.noreply.github.com>
2026-02-25 12:52:04 +0000

    fix: add button funcitonality to pricing buttons

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import gsap from 'gsap';
+import ScrollToPlugin from 'gsap/ScrollToPlugin';
 
 @Component({
   selector: 'app-pricing',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class PricingComponent {
 
+  constructor() {
+    gsap.registerPlugin(ScrollToPlugin);
+  }
+
+  scrollToContact() {
+    let contactSection = document.getElementById('contact-us-wrapper');
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: { y: contactSection, autoKill: true },
+    });
+  }
 }
